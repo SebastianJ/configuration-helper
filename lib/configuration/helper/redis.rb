5ce3068c3e4664b20d1adb40c8f2e3756bfb3fb6
Sebastian Johnsson <sebastian.johnsson@gmail.com>
2019-05-09 21:46:52 +0700

    Re-enable namespaces again... test with redis-rails without redis-namespace

--- lib/configuration/helper/redis.rb
+++ lib/configuration/helper/redis.rb
@@ -7,8 +7,28 @@ module Configuration
           ::Configuration::Helper::Utility.config_variable(:redis, var, default: default)
         end
     
-        def redis_url(database:)
+        def redis_url(database:, include_namespace: true)
           connection_string     =   "redis://#{config_variable(:password)}#{config_variable(:host)}:#{config_variable(:port)}/#{database}"
+      
+          if include_namespace
+            namespace           =   config_variable(:namespace)
+            connection_string   =   "#{connection_string}/#{namespace}" if use_namespace?(namespace)
+          end
+      
+          return connection_string
+        end
+    
+        def use_namespace?(namespace)
+          use_namespace             =   false
+          namespace_envs            =   config_variable(:namespace_environments)
+      
+          unless namespace_envs.to_s.empty?
+            use_namespace_for_envs  =   namespace_envs.split(",").collect(&:downcase)
+            use_namespace           =   use_namespace_for_envs.include?(Rails.env.downcase)
+            use_namespace           =   use_namespace && !namespace.to_s.empty?
+          end
+
+          return use_namespace
         end
     
         def configure_sessions(database: config_variable(:session_database).to_i, session_key: config_variable(:session_key), expire_in: 30.minutes)
@@ -25,18 +45,20 @@ module Configuration
           }
         end
     
-        def configure_sidekiq(server_pool_size: config_variable(:pool_size).to_i, client_pool_size: 1)
+        def configure_sidekiq(server_pool_size: config_variable(:pool_size).to_i, client_pool_size: 1, namespace: config_variable(:namespace))
           if defined?(Sidekiq)
-            configure_sidekiq_instance(type: :server, pool_size: server_pool_size)
-            configure_sidekiq_instance(type: :client, pool_size: client_pool_size)
+            configure_sidekiq_instance(type: :server, pool_size: server_pool_size, namespace: namespace)
+            configure_sidekiq_instance(type: :client, pool_size: client_pool_size, namespace: namespace)
           end
         end
     
-        def configure_sidekiq_instance(type: :server, database: config_variable(:sidekiq_database).to_i, pool_size: config_variable(:pool_size).to_i)
+        def configure_sidekiq_instance(type: :server, database: config_variable(:sidekiq_database).to_i, pool_size: config_variable(:pool_size).to_i, namespace: config_variable(:namespace))
           options   =   {
-            url:  redis_url(database: database),
+            url:  redis_url(database: database, include_namespace: false),
             size: pool_size,
           }
+      
+          options.merge!(namespace: namespace) if use_namespace?(namespace)
       
           Sidekiq.send("configure_#{type}") { |config| config.redis = options }
         end
