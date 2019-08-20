8d598752a10715458d2ea3f66062a070b2fc714c
Sebastian Johnsson <sebastian.johnsson@gmail.com>
2019-08-20 15:34:42 +0200

    Updates

--- lib/configuration/helper/redis.rb
+++ lib/configuration/helper/redis.rb
@@ -3,44 +3,50 @@ module Configuration
     class Redis
       
       class << self
-        def config_variable(var, default: nil)
-          ::Configuration::Helper::Utility.config_variable(:redis, var, default: default)
+        def config_variable(server, var, default: nil)
+          ::Configuration::Helper::Utility.config_variable(:redis, :servers, server, var, default: default)
         end
         
-        def connection_options(driver: nil, socket: nil, database: nil)
+        def connection_options(driver: nil, server: :master, database: nil)
           options               =   {}
           options[:driver]      =   driver unless driver.to_s.empty?
           
-          if !socket.to_s.empty?
-            options[:path]      =   socket
-          elsif !config_variable(:host).to_s.empty? && !config_variable(:port).to_s.empty?
-            options[:host]      =   config_variable(:host)
-            options[:port]      =   config_variable(:port)
+          if !config_variable(server, :path).to_s.empty?
+            options[:path]      =   config_variable(server, :path)
+          
+          elsif !config_variable(server, :host).to_s.empty? && !config_variable(server, :port).to_s.empty?
+            options[:host]      =   config_variable(server, :host)
+            options[:port]      =   config_variable(server, :port)
           end
           
-          options[:password]    =   config_variable(:password).gsub(/\@$/i, "") unless config_variable(:password).to_s.empty?
+          options[:password]    =   config_variable(server, :password).gsub(/\@$/i, "") unless config_variable(server, :password).to_s.empty?
           options[:db]          =   database unless database.to_s.empty?
           
           return options
         end
     
-        def redis_url(database:)
-          connection_string     =   "redis://#{config_variable(:password)}#{config_variable(:host)}:#{config_variable(:port)}/#{database}"
+        def redis_url(server: :master, database: nil)
+          connection_string     =   "redis://"
+          connection_string    +=   "#{config_variable(server, :password)}@" unless config_variable(server, :password).to_s.empty?
+          connection_string    +=   "#{config_variable(server, :host)}:#{config_variable(server, :port)}"
+          connection_string    +=   "/#{database}" unless database.to_s.empty?
+          
+          return connection_string
         end
     
-        def generate_cache_configuration(driver: nil, socket: config_variable(:cache_socket), database: config_variable(:cache_database).to_i)
-          return connection_options(driver: driver, socket: socket, database: database)
+        def generate_cache_configuration(driver: nil, server: :cache, database: config_variable(:master, :cache_database)&.to_i)
+          return connection_options(driver: driver, server: server, database: database)
         end
     
-        def configure_sidekiq(server_pool_size: config_variable(:pool_size).to_i, client_pool_size: 1)
+        def configure_sidekiq(server: :master, server_pool_size: config_variable(:master, :pool_size)&.to_i, client_pool_size: 1)
           if defined?(Sidekiq)
-            configure_sidekiq_instance(type: :server, pool_size: server_pool_size)
-            configure_sidekiq_instance(type: :client, pool_size: client_pool_size)
+            configure_sidekiq_instance(type: :server, server: server, pool_size: server_pool_size)
+            configure_sidekiq_instance(type: :client, server: server, pool_size: client_pool_size)
           end
         end
     
-        def configure_sidekiq_instance(type: :server, database: config_variable(:sidekiq_database).to_i, pool_size: config_variable(:pool_size).to_i)
-          options   =   connection_options.merge(size: pool_size)
+        def configure_sidekiq_instance(type: :server, server: :master, database: config_variable(:master, :sidekiq_database)&.to_i, pool_size: config_variable(:master, :pool_size)&.to_i)
+          options   =   connection_options(server: server, database: database).merge(size: pool_size)
           Sidekiq.send("configure_#{type}") { |config| config.redis = options }
         end
       end
